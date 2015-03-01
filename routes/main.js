4232019e59dd2369aed5e3b0aea5cda224a2b94a
Serj Babayan <serj.b.babayan@gmail.com>
2015-03-01 11:33:22 -0500

    Changed url endings to include a /

--- routes/main.js
+++ routes/main.js
@@ -1,36 +1,36 @@
 module.exports = function(app, passport) {
 
   app.get('/', isLoggedIn, function(req, res) {
-      res.redirect('/app'); //redirect to the app page
+      res.redirect('/app/'); //redirect to the app page
   });
 
-  app.get('/app',isLoggedIn, function(req, res) {
+  app.get('/app/',isLoggedIn, function(req, res) {
       res.render('app'); //renders the app page
   });
 
-  app.get('/login', function(req, res) {
+  app.get('/login/', function(req, res) {
     // render the page and pass in any flash data if it exists
     res.render('login', {
       message: req.flash('loginMessage')
     });
   });
 
-  app.get('/register', function(req, res) {
+  app.get('/register/', function(req, res) {
     // render the page and pass in any flash data if it exists
     res.render('register', {
       message: req.flash('signupMessage')
     });
   });
 
-  app.post('/register', passport.authenticate('local-signup', {
-    successRedirect: '/app', // redirect to the app page
-    failureRedirect: '/register', // redirect back to the signup page if there is an error
+  app.post('/register/', passport.authenticate('local-signup', {
+    successRedirect: '/app/', // redirect to the app page
+    failureRedirect: '/register/', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
 
-  app.get('/logout', function(req, res) {
+  app.get('/logout/', function(req, res) {
     req.logout(); //using the passport method for logging out
-    res.redirect('/login');
+    res.redirect('/login/');
   });
 };
 
@@ -42,5 +42,5 @@ function isLoggedIn(req, res, next) {
 	  return next();
 
   // if they aren't redirect them to the login page
-  res.redirect('/login');
+  res.redirect('/login/');
 }
