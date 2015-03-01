d3e5c9a0e3f8f29864571b40d36e1ac937cbf830
Serj Babayan <serj.b.babayan@gmail.com>
2015-03-01 12:59:13 -0500

    Added chatroom schema

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -1,8 +1,19 @@
+//loading required modules
+var mongoose = require('mongoose');
 
-  var ChatRoom=function ChatRoom(id,authorized_users){
-    this.id=id;
-    this.authorized_users=authorized_users;
-  };
+//Creating the schema for the user
+var ChatRoom = mongoose.Schema({
+  type: String, //can be public, private, or direct
+  authorized_users: [String], //id of all users allowed to read from and post to the chat room. Can be "ALL" for public
+  chat_history: [Message],
+  description: [String],
+  createdAt: Date,
+  name: String //the name of the chatroom given by the user
+  });
 
+ChatRoom.methods.addMessageHistory=function(Message){
+  this.chat_history.push(Message);
+};
 
-  module.exports=ChatRoom;
+// create the model for users and expose it to the app
+module.exports = mongoose.model('ChatRoom', ChatRoom);
