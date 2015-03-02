9ecfc601426a92ba78f4105e5c3fbfeb638b54af
Serj Babayan <sergeb@ca.ibm.com>
2015-03-02 11:41:42 -0500

    Refactored and added app script

---
+++ public/scripts/app.js
@@ -0,0 +1,16 @@
+
+
+$('form').submit(function() {
+  //Sends the message contents to the specified chatroom
+  socket.emit('message', $('#message-text-input').val());
+  $('#message-text-input').val('');
+  return false;
+});
+//Is called when it receives the 'chat message' event from the server,
+//after which is appends it to the html
+socket.on('message', function(msg) {
+  $('#messages-list').append($('<li class="messages">').text(msg));
+  $("#messages-list").animate({
+    "scrollTop": $('#messages-list')[0].scrollHeight
+  }, "slow");
+});
