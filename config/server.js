d96afb296de5b13859ebd8f1304fd4000d10b427
Serj Babayan <serj.b.babayan@gmail.com>
2015-03-07 16:39:35 -0500

    Created a server configuration module

---
+++ config/server.js
@@ -0,0 +1,7 @@
+//this file contains the most important server configuration options
+module.exports={
+  temporaryFilesLocation:"./tmp/", //where to store the temporary files of the server from form data
+  maxFileUploadSize:25000000, //the maximum size of a file a user can upload to the server
+  maxFileUploadNumber:1,//the maxmimum number of files a user can upload from a form
+
+};
