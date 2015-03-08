39910b28f3295ec25f3a2395f54f6d58b3337daa
Serj Babayan <serj.b.babayan@gmail.com>
2015-03-07 23:48:58 -0500

    Added average time event looped functionality for blocked test

--- tests/bench_event_loop.js
+++ tests/bench_event_loop.js
@@ -3,6 +3,10 @@ var blocked = require('blocked');
 var logger=require('../logger.js');
 
 module.exports = function() {
+  var num=0;
+  var avg=0;
+  var total=0;
+
   setInterval(function() {
     Array(10000000).join('a');
   }, 500);
@@ -12,6 +16,10 @@ module.exports = function() {
   }, 3000);
 
   blocked(function(ms) {
+    num++;
+    total+=ms;
+    avg=Math.round(total/num);
     logger.debug('EVENT LOOP BLOCKED FOR %sms', ms | 0);
+    logger.debug('AVERAGE TIME BLOCKED: '+avg+"ms");
   });
 };
