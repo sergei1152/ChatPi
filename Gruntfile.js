b78140d3802b037937878cab57eb1b34d044923d
Serge Babayan <serj.b.babayan@gmail.com>
2015-03-22 11:53:16 -0400

    Setup bower and all it's components

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,6 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt); //will load all the grunt plugins installed in the package.json
 
   grunt.initConfig({
-    //to read the package.json for app versions
-    pkg: grunt.file.readJSON('package.json'),
-
     //is used to start and restart the express server
     express: {
       dev: {
@@ -22,10 +19,8 @@ module.exports = function(grunt) {
         path: 'http://localhost:' + SERVER_SETTINGS.serverPort
       }
     },
-
     //to watch for file changes and execute tasks
     watch: {
-
       //will watch for any server side javascript file changes
       express: {
         files: ['./*.js', './routes/*.js', './models/*.js', './config/*.js', './static/*.js'], //Files to be watched
@@ -37,7 +32,7 @@ module.exports = function(grunt) {
       //will watch for any changes in client side javascript
       staticJS: {
         files: ['./public/scripts/*.js'], //Files to be watched
-        tasks: ['jshint:client', 'uglify:dist', 'concat:js'], //will hint the files and then minify and concatinate
+        tasks: ['jshint:client'], //will hint the files and then minify and concatinate
         options: { //Server options
           spawn: false, //Must have for reload
           livereload: true //Enable LiveReload
@@ -45,7 +40,7 @@ module.exports = function(grunt) {
       },
       staticCSS: {
         files: ['./public/styles/*.css'], //All css files in the styles directory
-        tasks: ['cssmin:dist', 'concat:css'], //will hint the files and then minify and concatinate
+        tasks: [], //will hint the files and then minify and concatinate
         options: { //Server options
           spawn: false, //Must have for reload
           livereload: true //Enable LiveReload
@@ -61,7 +56,6 @@ module.exports = function(grunt) {
         }
       }
     },
-
     // configure jshint to validate js files -----------------------------------
     jshint: {
       options: {
@@ -70,62 +64,7 @@ module.exports = function(grunt) {
       // when this task is run, lint the Gruntfile and all js files in project root
       server: ['./*.js', './routes/*.js', './models/*.js', './config/*.js', './static/*.js'],
       client: ['./public/scripts/*.js']
-    },
-
-    //for concatinating all the minified css and javascript libraries into one file
-    concat: {
-      options: {
-        // define a string to put between each file in the concatenated output
-        separator: ';'
-      },
-      libscript: {
-        // the files to concatenate
-        src: [
-          './bower_components/angular/angular.min.js',
-          './bower_components/jquery/dist/jquery.min.js',
-          './bower_components/bootstrap/dist/js/bootstrap.min.js',
-          './bower_components/sockjs-client/dist/sockjs.min.js'],
-        // the location of the resulting JS file
-        dest: './public/scripts/dist/libraries.min.js'
-      },
-      libcss: {
-        // the files to concatenate
-        src: ['src/**/*.js'],
-        // the location of the resulting JS file
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    //will minify all client side js files
-    uglify: {
-      options: {
-        banner: '/*!<%= pkg.Name %> V<%= pkg.Version %> \n <%= pkg.description %> \nMinified on: <%=grunt.template.today("dd-mm-yyyy") %> */\n',
-        mangle:false
-      },
-      //for minifying the libraries (done once since libraries don't really change often)
-      socketio: {
-        files: {
-          './bower_components/sockjs-client/dist/sockjs.min.js': ['./bower_components/sockjs-client/dist/sockjs.js']
-        }
-      },
-      //will minify all non-library or custom scripts
-      client: {
-        files: {
-          './public/scripts/dist/app.min.js': ['./public/scripts/*.js']
-        }
-      }
-    },
-    // configure cssmin to minify css files ------------------------------------
-    cssmin: {
-      options: {},
-      build: {
-        files: {
-          'public/style/dist/libraries.min.css': ['public/style/bootstrap.min.js', 'public/style/bootstrap-theme.min.css'],
-          'public/style/dist/app.style.min.css': 'public/style/app.css'
-        }
-      }
-    },
-
+    }
   });
-  grunt.registerTask('setup', ['uglify:socketio','concat:libscript','uglify:client']);
   grunt.registerTask('default', ['express:dev', 'open:dev', 'watch']);
 };
