624fd1cf4f66366ae05f238ccc1998d3efe54994
Serge Babayan <serj.b.babayan@gmail.com>
2015-03-29 11:15:46 -0400

    Implemented saving new messages history back to mongodb

--- models/PublicChannel.js
+++ models/PublicChannel.js
@@ -2,6 +2,9 @@
 var mongoose = require('mongoose');
 var Message=require('./Message.js'); //the Message schema
 
+var async=require('async');
+var SERVER_SETTINGS=require('../config/server-config.js');
+
 //Creating the schema for the user
 var PublicChannel = new mongoose.Schema({
   name: {
@@ -30,18 +33,21 @@ PublicChannel.methods.addMessageHistory=function(Message) {
 PublicChannel.methods.update=function(newChannel,callback){
   if(this.description!==newChannel.description){
     this.name=newChannel.description;
-    this.save(function(err){
-      if(err){
-        logger.error('There was an error in updating a public channel to the mongo database');
-      }
-    });
   }
-  //if(newChannel.chatHistory[newChannel.chatHistory.length-1].dateSentInMinutes>this.chatHistory[this.chatHistory.length-1].dateSentInMinutes){
-  //  //should change the message history here
-  //}
-  if(callback){
-    callback();
+  //if new messages have been saved to the chat history
+  if(newChannel.chat_history.length>SERVER_SETTINGS.numMessageToStore){
+    newChannel.chat_history.splice(0,SERVER_SETTINGS.numMessageToStore-1);
+    this.chat_history.splice(this.chat_history.length-1,0,newChannel.chat_history);
   }
+  this.save(function(err){
+    if(err){
+      logger.error('There was an error in updating a public channel to the mongo database');
+      callback(err);
+    }
+    else if(!err && callback){
+      callback();
+    }
+  });
 };
 
 // create the model for users and expose it to the app
