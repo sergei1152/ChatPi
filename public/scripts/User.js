4aee48f870efc55d5cf0ad9328ee72accf6bc47c
Serge Babayan <serj.b.babayan@gmail.com>
2015-03-29 22:55:29 -0400

    Fixed module load error

--- public/scripts/User.js
+++ public/scripts/User.js
@@ -1,5 +1,5 @@
 //This module has everything to do with the filters and directives responsible for the user experience
-var User=angular.module('UX',[]);
+var User=angular.module('User',[]);
 //service that returns the current date in minutes to be used in message dates
 User.service("User", function() {
   this.selfName='';
