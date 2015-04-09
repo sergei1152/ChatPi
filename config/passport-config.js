ec0ee2d5f00c52f5a9b590beb0275ac582bd62c9
Serge Babayan <serj.b.babayan@gmail.com>
2015-04-08 22:10:43 -0400

    Modularized the validation functions

--- config/passport-config.js
+++ config/passport-config.js
@@ -29,80 +29,81 @@ module.exports = function(passport,RedisClient) {
 
   //passport strategy for local signups
   passport.use('signup', new LocalStrategy({
-      usernameField: 'username',
-      passwordField: 'password',
-      passReqToCallback: true // allows us to pass back the entire request to the callback
-    },
-    function(req, username, password, done) {
-      // asynchronous
-      // User.findOne wont fire unless data is sent back
-      process.nextTick(function() {
-        //validating all the fields, including the profile picture (if exists)
-        if (validator(req, "signup")) {
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true // allows us to pass back the entire request to the callback
+  },
+  function(req, username, password, done) {
+    // asynchronous
+    // User.findOne wont fire unless data is sent back
+    process.nextTick(function() {
+      //validating all the fields, including the profile picture (if exists)
+      if (validator.validateRegistration(req)){
 
-          //searches the mongodb database for the username
-          User.findOne({
-            'username': username
-          }, function(err, user) {
-            // if there are any errors, return the error
-            if (err) {
-              logger.error("An error occured while retrieving usernames from the mongo database at the register screen\n %j", {
-                'error': error
-              }, {});
-              return done(err);
-            }
-            // if there is an existing user with the same username
-            if (user) {
-              return done(null, false, req.flash('signUpMessage', 'That username is already taken'));
-            }
-            //if the username is unique
-            var newUser = new User();
+        //searches the mongodb database for the username
+        User.findOne({
+          'username': username
+        }, function(err, user) {
+          // if there are any errors, return the error
+          if (err) {
+            logger.error("An error occured while retrieving usernames from the mongo database at the register screen\n %j", {
+              'error': error
+            }, {});
+            return done(err);
+          }
+          // if there is an existing user with the same username
+          if (user) {
+            return done(null, false, req.flash('signUpMessage', 'That username is already taken'));
+          }
+          //if the username is unique
+          var newUser = new User();
 
-            // set the user's local credentials
-            newUser.username = username;
-            newUser.password = newUser.generateHash(password);
-            newUser.createdAt = Date.now();
-            newUser.name = req.body.name;
+          // set the user's local credentials
+          newUser.username = username;
+          newUser.password = newUser.generateHash(password);
+          newUser.createdAt = Date.now();
+          newUser.name = req.body.name;
 
-            //if a profile picture was uploaded
-            if (req.files.profile_picture) {
-              var imagePath = req.files.profile_picture.path;
-              var newPath = path.join(__dirname, "../public/resources/profiles/" + username + "." + req.files.profile_picture.extension);
+          //if a profile picture was uploaded
+          if (req.files.profile_picture) {
+            var imagePath = req.files.profile_picture.path;
+            var newPath = path.join(__dirname, "../public/resources/profiles/" + username + "." + req.files.profile_picture.extension);
 
-              //moves the profile picture to a permanent directory
-              fs.rename(imagePath, newPath, function(err) {
-                if (err) {
-                  logger.error('There was an error in reading a profile picture from the /tmp/ folder\n %j', {
-                    'error': err
-                  }, {});
-                  return done(null, false, req.flash('signUpMessage', 'Error saving your profile pic. Please try again.'));
-                }
-                newUser.profile_picture = username + "." + req.files.profile_picture.extension;
-                return saveUser(newUser, done);
-              });
-            } else {
-              newUser.profile_picture = "default.png";
-              saveUserRedis(newUser, RedisClient); //saving the user to the redis database
+            //moves the profile picture to a permanent directory
+            fs.rename(imagePath, newPath, function(err) {
+              if (err) {
+                logger.error('There was an error in reading a profile picture from the /tmp/ folder\n %j', {
+                  'error': err
+                }, {});
+                return done(null, false, req.flash('signUpMessage', 'Error saving your profile pic. Please try again.'));
+              }
+              newUser.profile_picture = username + "." + req.files.profile_picture.extension;
               return saveUser(newUser, done);
-            }
-          });
-        } else { //if the validation failed
-          return done(null, false, req.flash('signUpMessage', 'Validation failed.'));
-        }
-      });
-    }));
+            });
+          } else {
+            newUser.profile_picture = "default.png";
+            saveUserRedis(newUser, RedisClient); //saving the user to the redis database
+            return saveUser(newUser, done);
+          }
+        });
+      } else { //if the validation failed
+        return done(null, false, req.flash('signUpMessage', 'Validation failed.'));
+      }
+    });
+  }));
 
 
   passport.use('login', new LocalStrategy({
-      // by default, local strategy uses username and password, we will override with email
-      usernameField: 'username',
-      passwordField: 'password',
-      passReqToCallback: true // allows us to pass back the entire request to the callback
-    },
-    function(req, username, password, done) { // callback with email and password from our form
+    // by default, local strategy uses username and password, we will override with email
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true // allows us to pass back the entire request to the callback
+  },
+  function(req, username, password, done) { // callback with email and password from our form
 
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
+    //If server side validation is passed
+    if(validator.validateLogin(req)){
+      //Find the user in the mongodb database
       User.findOne({
         'username': username
       }, function(err, user) {
@@ -125,15 +126,19 @@ module.exports = function(passport,RedisClient) {
         });
         return done(null, user);
       });
-    }));
-};
-//saves the User schema object ot the database, logging any errors that heppened
-function saveUser(user,done){
-  user.save(function(err) {
-    if (err) {
-      logger.error("There was an error saving a new users info into the database \n %j", err, {});
-      return done(null, false, req.flash('signUpMessage', 'Error saving your stuff. Please try again'));
     }
-    return done(null, user);
-  });
-}
+    else { //if the validation failed
+      return done(null, false, req.flash('loginMessage','Please make sure your username and password are valid'));
+    }    
+    }));
+  };
+  //saves the User schema object ot the database, logging any errors that heppened
+  function saveUser(user,done){
+    user.save(function(err) {
+      if (err) {
+        logger.error("There was an error saving a new users info into the database \n %j", err, {});
+        return done(null, false, req.flash('signUpMessage', 'Error saving your stuff. Please try again'));
+      }
+      return done(null, user);
+    });
+  }
--- static/validator.js
+++ static/validator.js
@@ -1,45 +1,63 @@
-//Does validation for the login and register form
-var validatePicture=require('./pictureValidator.js');
+//Universal server side validation module that does validation for both usernames, passwords, and names (including public channel and private group names)
+var logger=require('../logger.js');//for pretty console outputs
+var SERVER_SETTINGS = require('../config/server-config.js');
 
 var regUsername=/^[a-zA-Z0-9_-]{4,16}$/; //must contain at least 4 characters up to 16, can contain numbers, letters, and _ and -
-var regPassword=/^[a-z0-9A-Z!$%^&+=*()@#_-]{8,128}$/;//password must be at least 8 characters long up to 128 and can have special characters
-var regName=/^[a-zA-Z ]+$/i; //only letter characters allowed
-module.exports=function(req,type){
+var regPassword=/^[a-z0-9A-Z!$%^&+=*()@#_ -]{8,128}$/;//password must be at least 8 characters long up to 128 and can have special characters (including spaces)
+var regName=/^[a-zA-Z ]+$/i; //only letter characters and spaces allowed
+var regChannel=/^[a-zA-Z #]+$/i; //only letter characters,hastags, and spaces allowed
 
-  if(type=="login"){
-    //check to see if a username and password field was passed in
-    if(req.body.username && req.body.password){
-      //make sure that both the username and password are valid
-      if(req.body.username.toString().match(regUsername)&&req.body.password.toString().match(regPassword)){
-        return true;
-      }
-      return false;
+module.exports.validateProfilePicture=function(req){
+  //if the profile picture is under the specified size and if it has a valid extension
+  if (req.files.profile_picture.size <= SERVER_SETTINGS.maxProfileImageSize && SERVER_SETTINGS.supportedProfileImageTypes.indexOf(req.files.profile_picture.extension) != -1) {
+    return true;
+  }
+  logger.debug("A user tried registering however the profile picture did not pass validation");
+  return false;
+};
+
+module.exports.validateLogin=function(req){
+  //Make sure that a username and password was submitted in the form
+  if(req.body && req.body.username && req.body.password){
+    //Validate the username and password with regex
+    if(req.body.username.toString().match(regUsername)&&req.body.password.toString().match(regPassword)){
+      return true;
     }
+    logger.debug("A user tried to login however the username/password did not pass validation");
     return false;
   }
-  else if (type=="signup"){
-    console.log("using sign up strategy");
-    if(req.body.username && req.body.password && req.body.name){
+  logger.debug("A user tried to login with empty username and password");
+  return false;
+};
 
-      //if a file was uploaded
-      if(req.files.profile_picture){
-        console.log(req.files);
-        console.log("picture found");
-        if(!validatePicture(req.files)){ //validate the uploaded picture
-          console.log("picture validation fails");
-          return false; //return false if the picture is not proper
-        }
+module.exports.validateRegistration=function(req){
+  //Make sure that a username, password and name were submitted
+  if(req.body && req.body.username && req.body.password && req.body.name){
+    //if a file(profile picture) was uploaded
+    if(req.files && req.files.profile_picture){
+      if(!module.exports.validateProfilePicture(req)){ //validate the uploaded picture
+        return false; 
       }
-      //make sure that both the username and password and name are valid
-      if(req.body.username.toString().match(regUsername)&&req.body.password.toString().match(regPassword) && req.body.name.toString().match(regName)){
-        return true;
-      }
-      console.log("username name or password invalid regex");
-      return false;
     }
-    console.log("password or username or name does not exist");
+    //make sure that both the username and password and name are valid
+    if(req.body.username.toString().match(regUsername)&&req.body.password.toString().match(regPassword) && req.body.name.toString().match(regName)){
+      return true;
+    }
+    logger.debug("A user tried to register however the name/username/password did not pass validation");
     return false;
   }
-  console.log("type not defined");
+  logger.debug("A user tried to register with an empty username/name/password");
+  return false;
+};
+
+module.exports.validatePublicChannel=function(channel){
+  if (channel.name){
+    if (channal.name.toString().match(regChannel)){
+      return true;
+    }
+    logger.debug("A user tried to create a new channel however validation for the channel name failed");
+    return false;
+  }
+  logger.debug("A user tried to create a public channel however did not submit a channel name");
   return false;
 };
