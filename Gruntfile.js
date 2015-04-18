948bb2171fd9369ce5b308c8f895697444e63e38
Serge Babayan <serj.b.babayan@gmail.com>
2015-04-18 11:27:56 -0400

    Fixed watch task for the new lib folder  (Grunt)

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
     watch: {
       //will watch for any server side javascript file changes
       express: {
-        files: ['./*.js', './routes/*.js', './models/*.js', './config/*.js', './lib/*.js'], //Files to be watched
+        files: ['./*.js', './routes/*.js', './models/*.js', './config/*.js', './lib/**/*.js'], //Files to be watched
         tasks: ['jshint:server', 'express:dev'], //(Re)start the server
         options: { //Server options
           spawn: false //Must have for reload
