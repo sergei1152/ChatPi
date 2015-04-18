f378778858af3396aaec0a19afb501294cd0902e
Serge Babayan <serj.b.babayan@gmail.com>
2015-04-18 13:37:36 -0400

    Added Private Groups mongoose model

---
+++ models/PrivateGroup.js
@@ -0,0 +1,34 @@
+//loading required modules
+var mongoose = require('mongoose');
+var MessageSchema=require('./Message.js').schema; //the Message schema
+var logger=require('../logger.js');
+
+//Creating the schema for the user
+var PrivateGroup = new mongoose.Schema({
+  name: {
+    type:String,
+    required: true
+  },
+  description:{
+    type:String,
+    default: ''
+  },
+  createdAt: {
+    type:Date,
+    default: Date.now
+  },
+  chat_history: {
+    type:[MessageSchema],
+    default:[]
+  },
+  authorized_users: {
+    type: [String],
+    default: []
+  }
+});
+
+// create the model for users and expose it to the app
+module.exports = mongoose.model('PrivateGroup', PrivateGroup);
+
+
+
